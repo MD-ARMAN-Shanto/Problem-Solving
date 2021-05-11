dbd458842ba09a2f41e099ee9f85e98538e8c958
arman <arman@evaly.com.bd>
2021-05-11 13:43:22 +0600

    string comparision

--- day_4.py
+++ day_4.py
@@ -41,3 +41,32 @@ nums = [1, 1, 2, 3, 4]
 val = 1
 final_result = removeElement(nums, val)
 print(final_result)
+
+# find out the length of common string between two string
+
+
+def strStr(haystack: str, needle: str) -> int:
+    return haystack.find(needle)
+
+
+if __name__ == '__main__':
+    haystack = "hello"
+    needle = "ll"
+    expected_ans = 2
+    final_result = strStr(haystack, needle)
+    assert expected_ans == final_result, final_result
+    print(final_result)
+
+    haystack = "aaaaaaaaa"
+    needle = "bba"
+    expected_ans = -1
+    final_result = strStr(haystack, needle)
+    assert expected_ans == final_result, final_result
+    print(final_result)
+
+    haystack = ""
+    needle = ""
+    expected_ans = 0
+    final_result = strStr(haystack, needle)
+    assert expected_ans == final_result, final_result
+    print(final_result)
