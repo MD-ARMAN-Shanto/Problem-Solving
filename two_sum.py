79b4b499e202c3bbeca16cdc167ce598995ced75
arman <hossain.arman@northsouth.edu>
2021-07-21 00:52:01 +0600

    two pointer approch

--- two_sum.py
+++ two_sum.py
@@ -1,7 +1,5 @@
-
-# find out the at least one pair/two integer numbers/position of the two integer numbers from an array which number of sum equals to
-# the target numbers
-# if target number is not found in the array return an empty array
+# find out the at least one pair/two integer numbers/position of the two integer numbers from an array which number
+# of sum equals to the target numbers if target number is not found in the array return an empty array
 
 # solution 1 : brute force
 
@@ -38,5 +36,57 @@ if __name__ == '__main__':
     expected_result = []
     assert expected_result == func_call, func_call
     print(func_call)
+
+
+# adding two sum with better approach
+
+# two pointer approach
+
+#Todo
+# step 1: first sort the given array
+# step 2: take start and end(upto it's length) position from the array
+# step 3: take a while loop until start < end
+# step 4: if arr[start] + arr[end] == target, return value
+# step 5: if sum > target then end -+ 1 else sum < target then  start +=1
+
+def two_sum_pointer(arr: List[int], target: int) -> List[int]:
+
+    arr.sort()
+
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        sum: int = arr[start] + arr[end]
+        if sum == target:
+            return [arr[start], arr[end]]
+
+        if sum > target:
+            end -= 1
+        elif sum < target:
+            start += 1
+    return []
+
+
+if __name__ == '__main__':
+    arr, target = [1, 4, 6, 7], 13
+    func_call = two_sum_pointer(arr, target)
+    expected_result = [6, 7]
+    assert expected_result == func_call, func_call
+    print(func_call)
+
+    arr, target = [4, 6, 7, -4], 0
+    func_call = two_sum_pointer(arr, target)
+    expected_result = [-4, 4]
+    assert expected_result == func_call, func_call
+    print(func_call)
+
+    arr, target = [1, 4, 4, 3], 10
+    func_call = two_sum_pointer(arr, target)
+    expected_result = []
+    assert expected_result == func_call, func_call
+    print(func_call)
+
+
 
 
