541554ad62d4ef60afdf65bc655b8661a9fb1a19
arman <hossain.arman@northsouth.edu>
2021-09-04 13:09:41 +0600

    add two sum using linked list

---
+++ Linked_List/addTwoSum.py
@@ -0,0 +1,64 @@
+"""
+Approach: linearSearch
+Status: Accepted
+131 / 131 test cases passed.
+Runtime: 96 ms
+Memory Usage: 15.6 MB
+Problem link: https://leetcode.com/problems/add-two-numbers/
+Time complexity: O(n)
+space complexity: O(1)
+"""
+
+
+class ListNode(object):
+
+    def __init__(self, data=0, next=None):
+        self.data = data
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        nHead, h1, h2 = None, l1, l2
+        run = None
+        c = 0
+        while h1 and h2:
+            s = h1.val + h2.val + c
+            c = s // 10
+            s = s % 10
+            temp = ListNode(s)
+
+            if not nHead:
+                nHead = temp
+                run = temp
+            else:
+                run.next = temp # assign head next address to next val
+                run = run.next  # assign new run address
+            h1 = h1.next
+            h2 = h2.next
+
+        # if h1 has more nodes than h2
+        while h1:
+            s = h1.val + c
+            c = s // 10
+            s = s % 10
+            run.next = ListNode(s)
+            run = run.next
+            h1 = h1.next
+
+        # if h2 has more nodes than h1
+        while h2:
+            s = h2.val + c
+            c = s // 10
+            s = s % 10
+            run.next = ListNode(s)
+            run = run.next
+            h2 = h2.next
+
+        # no nodes left but we have the only carry value 1 exists
+        if c != 0:
+            run.next = ListNode(c)
+            run = run.next
+
+        return nHead
