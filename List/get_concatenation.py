dd07c3332fa266c2c99819d6e8a931037801ddb9
arman <hossain.arman@northsouth.edu>
2021-11-23 01:06:40 +0600

    add test cases

--- List/get_concatenation.py
+++ List/get_concatenation.py
@@ -13,3 +13,23 @@ def getConcatenation(nums: List[int]) -> List[int]:
     nums1 = nums.copy()
     res = nums1 + nums
     return res
+
+
+if __name__ == '__main__':
+    nums1 = [1, 2, 3]
+    fc = getConcatenation(nums1)
+    expected_result = [1, 2, 3, 1, 2, 3]
+    assert expected_result == fc, fc
+    print(fc)
+
+    nums1 = [0]
+    fc = getConcatenation(nums1)
+    expected_result = [0, 0]
+    assert expected_result == fc, fc
+    print(fc)
+
+    nums1 = []
+    fc = getConcatenation(nums1)
+    expected_result = []
+    assert expected_result == fc, fc
+    print(fc)
