96da079e35584ac341ac9948126e50c717f603e4
arman <hossain.arman@northsouth.edu>
2021-12-27 02:28:01 +0600

    update parity code

--- sort-array-by-parity/sort-array-by-parity.py
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -1,21 +1,17 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        
+
         start = 0
-        end = len(nums)-1
-        
+        end = len(nums) - 1
+
         while start < end:
-            if nums[start] % 2 != 0 and nums[end] % 2 != 0:
-                end -= 1
-            elif nums[start] % 2 == 0 and nums[end] % 2 == 0:
+            if nums[start] % 2 == 0:
                 start += 1
-            elif nums[start] % 2 == 0 and nums[end] % 2 != 0:
+            elif nums[end] % 2 != 0:
                 end -= 1
             else:
                 nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-        
+
         return nums
                 
         
