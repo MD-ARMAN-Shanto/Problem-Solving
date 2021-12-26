5eb7f816438ac50c2bfad7ce37a222fdff2cd4db
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2021-12-27 00:11:37 +0600

    Time: 216 ms (96.20%), Space: 16.1 MB (6.62%) - LeetHub

---
+++ decode-xored-array/decode-xored-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        
+        arr = [first]
+
+        for i in encoded:
+            arr.append(arr[-1] ^ i)
+            
+        return arr
