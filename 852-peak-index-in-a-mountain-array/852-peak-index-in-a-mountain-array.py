0ea96013b754c915d5ae725d0454792fc10aa3bd
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-08 13:01:11 +0600

    updated peak moutain array with another logic

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -26,4 +26,17 @@ class Solution:
             mid = (start+end) // 2
             
         return mid+1
+    
+        # approch 3
+        start = 0
+        end = len(arr) - 1
+
+        while start < end:
+            mid = (start+end) // 2
+            if arr[mid] > arr[mid+1]:
+                end = mid 
+            else:
+                start = mid + 1
+            
+        return start
 
