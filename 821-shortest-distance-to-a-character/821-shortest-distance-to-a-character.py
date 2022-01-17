2331881cb9933805f7609aeb4df6297ee7cbd881
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-17 23:08:51 +0600

    Time: 113 ms (11.66%), Space: 14.3 MB (58.75%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -6,15 +6,10 @@ class Solution:
         for i in range(len(s)):
             if s[i] == c:
                 occurance.append(i)
-        _ = []
+
         res = []
         for ch in range(len(s)):
-            for r in occurance:
-                min_val = abs(r-ch)
-                _.append(min_val)
-                # res.append(min(abs(r-ch) for r in occurance))
-            res.append(min(_))
-            _[:] = []
+            res.append(min(abs(r-ch) for r in occurance))
                 
         return res
             
