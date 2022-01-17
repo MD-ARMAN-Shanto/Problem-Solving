756ed1cc7c5968373be68d6187f3a596ef7a3a2c
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-17 23:11:38 +0600

    try another way to solve the problem

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -6,6 +6,24 @@ class Solution:
         for i in range(len(s)):
             if s[i] == c:
                 occurance.append(i)
+        _ = []
+        res = []
+        for ch in range(len(s)):
+            for r in occurance:
+                min_val = abs(r-ch)
+                _.append(min_val)
+                # res.append(min(abs(r-ch) for r in occurance))
+            res.append(min(_))
+            _[:] = []
+                
+        return res
+        
+        
+        occurance = []
+        
+        for i in range(len(s)):
+            if s[i] == c:
+                occurance.append(i)
 
         res = []
         for ch in range(len(s)):
