d16f489b463e6c500be93a40f792e99b5209716e
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-19 16:10:48 +0600

    Time: 298 ms (29.53%), Space: 16.3 MB (73.46%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -1,18 +1,26 @@
 class Solution:
-    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
         
-        even_arr = []
-        odd_arr = []
+#         even_arr = []
+#         odd_arr = []
         
-        for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                even_arr.append(nums[i])
-            else:
-                odd_arr.append(nums[i])
+#         for i in range(len(nums)):
+#             if nums[i] % 2 == 0:
+#                 even_arr.append(nums[i])
+#             else:
+#                 odd_arr.append(nums[i])
                 
-        res = []
-        for j in range(len(even_arr)):
-            res.append(even_arr[j])
-            res.append(odd_arr[j])
+#         res = []
+#         for j in range(len(even_arr)):
+#             res.append(even_arr[j])
+#             res.append(odd_arr[j])
             
-        return res
+#         return res
+
+        j = 1
+        for i in range(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
+        return A
