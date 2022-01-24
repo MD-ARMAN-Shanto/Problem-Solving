c1af54cebaf4b4443c8d8deae210eafec0e97e18
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-25 02:04:58 +0600

    Time: 579 ms (81.08%), Space: 22 MB (43.24%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,9 +1,42 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
+        # approch 1 builtin stl method
+        # return sorted(nums)
+        
+        # approch 2 priority queue
+        
         h = []
+        res = []
         for value in nums:
             heapq.heappush(h, value)
         
-        return [heappop(h) for i in range(len(h))]
+        return [heapq.heappop(h) for i in range(len(h))]
+    
+        # approch 3 using recursion for small length
+
+#         if len(nums) == 0:
+#             return
+        
+#         temp = nums[-1]
+#         nums.pop()
+        
+#         self.sortArray(nums)
+#         self.insertSort(nums, temp)
+        
+#         return nums
+        
+#     def insertSort(self, arr: List[int], temp: int):
+        
+#         if len(arr) == 0 or temp >= arr[-1]:
+#             arr.append(temp)
+#             return
+        
+#         tmp = arr[-1]
+#         arr.pop()
+        
+#         self.insertSort(arr, temp)
+#         arr.append(tmp)
+        
+        
         
