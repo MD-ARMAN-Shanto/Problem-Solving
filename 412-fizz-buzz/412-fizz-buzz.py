208d3c87d10fd14c24c8c5947322ff0525b920d1
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-26 01:02:55 +0600

    Time: 63 ms (28.57%), Space: 15.1 MB (24.15%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -9,11 +9,11 @@ class Solution:
             if i % 3 == 0 and i % 5 == 0:
                 output.append('FizzBuzz')
             
-            elif i % 5 == 0:
-                output.append('Buzz')
-                
             elif i % 3 == 0:
                 output.append('Fizz')
+                
+            elif i % 5 == 0:
+                output.append('Buzz')
             else:
                 output.append(str(i))
             
