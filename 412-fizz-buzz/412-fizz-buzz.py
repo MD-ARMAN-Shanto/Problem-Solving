91da6f407cb3f657211a32631b0db25fb6b2d109
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-01-26 00:59:21 +0600

    Time: 72 ms (17.75%), Space: 15.2 MB (24.15%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,21 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        
+        output = []
+        
+        for i in range(n):
+            i = i + 1
+            
+            if i % 3 == 0 and i % 5 == 0:
+                output.append('FizzBuzz')
+            
+            elif i % 5 == 0:
+                output.append('Buzz')
+                
+            elif i % 3 == 0:
+                output.append('Fizz')
+            else:
+                output.append(str(i))
+            
+        return output
+        
