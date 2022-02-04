bba1eebcf88c7161dd774161c77585a73f6dd41e
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-02-04 22:37:21 +0600

    Time: 145 ms (61.10%), Space: 16.8 MB (24.16%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
+        # counts = {}
+        
+        # for n in nums:
+        # if n not in counts:
+        #  counts[n] = 1
+        # else :
+        # del counts[n]
+                
+        # return list(counts.keys())[0]
         a = 0
     
         for i in nums:
