0e4feb50466e1eea929e718f168311fa1efa6854
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-02-18 20:37:38 +0600

    Time: 53 ms (70.20%), Space: 13.9 MB (92.46%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,4 +1,31 @@
 class Solution:
+    
+    def helper(self, number: str) -> int: 
+        
+        intDict = {
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9
+        }
+
+        num = 0
+        for n in number:
+            num *= 10
+            num += intDict[n]
+
+        return num
+            
+    
+        
     def multiply(self, num1: str, num2: str) -> str:
         
-        return str(int(num1)*int(num2))
+        return str(self.helper(num1) * self.helper(num2))
+        
+        
