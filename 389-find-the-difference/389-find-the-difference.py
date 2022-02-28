551168c2f6c6234ffe79d3f0054d18bc023ffc8a
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-02-28 23:03:09 +0600

    Time: 39 ms (77.28%), Space: 14 MB (40.95%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,11 +1,15 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         
-        l1 = list(s)
+        # l1 = list(s)
         
-        for i in range(len(t)):
-            if t[i] in l1:
-                l1.remove(t[i])
-            else:
-                return t[i]
+        # for i in range(len(t)):
+        #     if t[i] in l1:
+        #         l1.remove(t[i])
+        #     else:
+        #         return t[i]
+        
+        for i in t:
+            if s.count(i) != t.count(i):
+                return i
                     
