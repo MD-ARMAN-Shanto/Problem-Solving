578e91b72ccc7f79405bfaf9f02b9b20c9459454
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-03-02 23:01:12 +0600

    Time: 32 ms (90.25%), Space: 14 MB (43.67%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,15 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-        # if len(digits)==0:
-        #     # fail safe
-        #     return digits
-        # else:
-        #     # increment the number to a single integer
-        #     number = int(''.join([str(k) for k in digits]))+1
-        #     # return back a list representation
-        #     return [int(k) for k in str(number)]
-        
         if len(digits) == 0:
             return digits
         
