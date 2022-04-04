c1ac3f6975b0dfcf04870169bb89fd965e5dc2a0
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-04-04 13:57:40 +0600

    Time: 204 ms (12.09%), Space: 18.5 MB (98.63%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        
+        d = {}
+        
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+            
+        res = list(d.values())
+        
+        rev_list = sorted(res, reverse=True)[:k]
+        
+        
+        final_result = []
+
+        for i in range(len(rev_list)):
+            if rev_list[i] in d.values():
+                position = list(d.values()).index(rev_list[i])
+                f_n = list(d.keys())[position]           
+                final_result.append(f_n)
+                del d[list(d.keys())[position]]
+    
+        return final_result
