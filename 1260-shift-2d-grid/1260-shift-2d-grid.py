0547141a47f42a2aba26c64d0ff4d4305d975fc7
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-04-11 13:10:56 +0600

    Time: 410 ms (23.39%), Space: 14.3 MB (35.10%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,21 +1,20 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         
-        M, N = len(grid), len(grid[0]) # take number of rows and number of columns
-        
-        # making 2D to 1D 
+        M, N = len(grid), len(grid[0]) 
+
         def posToVal(r, c):
             return r * N + c
         
-        # making 1D to 2D matrix
+
         def valToPos(v):
-            return [ v // N, v % N] # 1st making row then column
+            return [ v // N, v % N] 
         
         res = [[0] * N for i in range(M)]
         
         for r in range(M):
             for c in range(N):
-                newVal = (posToVal(r, c) + k) % (M * N) # for not raising bound error
+                newVal = (posToVal(r, c) + k) % (M * N) 
                 row, column = valToPos(newVal)
                 res[row][column] = grid[r][c]
                 
