f6e57e12653d979863c2f326befa827ee4b596be
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-04-13 00:54:08 +0600

    Time: 84 ms (5.32%), Space: 14.1 MB (12.69%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.py
@@ -0,0 +1,45 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        # original | new | state
+        #    0     |  0  |   0
+        #    1     |  0  |   1
+        #    0     |  1  |   2
+        #    1     |  1  |   3
+        
+        Rows, Cols = len(board), len(board[0])
+        
+        def countNeighbours(r, c):
+            nei = 0
+            
+            for i in range(r-1, r+2):
+                for j in range(c-1, c+2):
+                    if ((i==r and j==c) or i<0 or j<0 or i==Rows or j==Cols):
+                        continue
+                    if board[i][j] in [1,3]:
+                        nei += 1
+            return nei
+        
+        
+        for r in range(Rows):
+            for c in range(Cols):
+                nei = countNeighbours(r, c)
+                
+                if board[r][c]:
+                    if nei in [2, 3]:
+                        board[r][c] = 3
+                        
+                elif nei == 3:
+                    board[r][c] = 2
+                    
+        
+        for r in range(Rows):
+            for c in range(Cols):
+                if board[r][c] == 1:
+                    board[r][c] = 0
+                elif board[r][c] in [2,3]:
+                    board[r][c] = 1
+               
