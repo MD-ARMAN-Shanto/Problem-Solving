86bdebb5f9d4d7eac4f5980b197f371bbb7b2f99
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-05-10 12:20:28 +0600

    Time: 30 ms (85.52%), Space: 13.7 MB (95.90%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -17,4 +17,4 @@ class Solution:
         bin_number = bin(num)[2:]
         masking = int(math.pow(2, len(bin_number))) -1
         
-        return num ^ masking
+        return abs(num - masking)
