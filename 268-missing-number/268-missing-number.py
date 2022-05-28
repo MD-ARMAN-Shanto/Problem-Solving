bc790d42d049ba72dab504978291e7e6c2f381ad
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-05-28 10:52:01 +0600

    Update 268-missing-number.py
    add one-liner solution

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
+        # return len(nums)*(len(nums)+1)//2 - sum(nums)
+        
         d = {}
         
         for num in nums:
