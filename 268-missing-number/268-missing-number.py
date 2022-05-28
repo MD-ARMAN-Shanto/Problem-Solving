7f57bc48bab9d0ed4ee75648d51cc05aa7b2bb82
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-05-28 10:50:15 +0600

    Update 268-missing-number.py
    Runtime: 190 ms, faster than 49.55% of Python3 online submissions for Missing Number.
    Memory Usage: 15.6 MB, less than 10.55% of Python3 online submissions for Missing Number.

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,8 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        for i in range(len(nums)+1):
-            if i not in nums:
+        d = {}
+        
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+            
+        for i in range(len(nums) + 1):
+            if i not in d:
                 return i
             
         
