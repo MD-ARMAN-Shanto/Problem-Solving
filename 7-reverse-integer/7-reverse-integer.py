f22c1c9085cc378691479c8ccaa86151dae8252b
arman <hossain.arman@northsouth.edu>
2022-05-29 20:12:20 +0600

    update variable

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x==0:
+        if x == 0:
             return x
         if x > 0:
             rev = int(str(x)[::-1])
@@ -9,4 +9,5 @@ class Solution:
 
         if rev > 2147483648 or rev < -2147483648:
             return 0
+
         return rev
