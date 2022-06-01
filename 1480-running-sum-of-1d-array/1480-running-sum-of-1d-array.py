6efe9e5b85565f5300a6659660964229e1c57144
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-06-01 11:25:49 +0600

    Time: 76 ms (19.31%), Space: 14.3 MB (27.03%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         
-        # res = [nums[i]*(nums[i]+1)/2 for i in range(len(nums)]
         res = []
         s = 0
         for i in range(len(nums)):
