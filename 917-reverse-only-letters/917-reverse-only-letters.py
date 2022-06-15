82326657bdc14b34bb2777fe937a480d58d885d5
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-06-15 13:36:51 +0600

    Time: 43 ms (51.56%), Space: 13.9 MB (62.11%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.py
+++ 917-reverse-only-letters/917-reverse-only-letters.py
@@ -5,18 +5,16 @@ class Solution:
         i, j = 0, len(l)-1
                 
         while i < j:
-            if l[i].isalpha() and l[j].isalpha():
-                l[i], l[j] = l[j], l[i]
+            
+            if not l[i].isalpha():
+                i += 1
+                continue
                 
-                
-            elif l[i].isalpha() and not l[j].isalpha():
+            if not l[j].isalpha():
                 j -= 1
                 continue
                 
-            elif not l[i].isalpha() and l[j].isalpha():
-                i += 1
-                continue
-            
+            l[i], l[j] = l[j], l[i]
             i += 1
             j -= 1
             
