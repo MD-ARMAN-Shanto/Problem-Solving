990af9532b08179b7e7ef48f27ec2426ee5a24c9
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-06-22 12:47:08 +0600

    Time: 1424 ms (5.05%), Space: 14.6 MB (95.16%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -8,12 +8,10 @@ class Solution:
         for j in range(len(nums)):
             if nums[j] == 0:
                 zeroCount += 1
-                
             while zeroCount > k:
                 if nums[i] == 0:
                     zeroCount -= 1
                 i += 1
-                    
             result = max(result, j-i+1)
     
         return result
