d79c2ceaec62e7348f2df7ee261b480d83682004
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-06-26 13:37:13 +0600

    Time: 493 ms (73.17%), Space: 27 MB (95.20%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxScore(self, cp: List[int], k: int) -> int:
+        
+        # find minimum value from sub array
+        
+        N = len(cp)
+        M = N-k
+        total = sum(cp)
+        mn = s = sum(cp[:M])
+        
+        for i in range(k):
+            s -= cp[i]
+            s += cp[i+M]
+            mn = min(mn, s)
+        
+        return total - mn
+            
+            
+            
+            
+                
