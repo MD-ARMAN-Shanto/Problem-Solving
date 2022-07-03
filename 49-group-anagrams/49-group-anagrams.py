1b72be29a50301429faf13440f8bd15cb15e717a
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-07-03 22:42:47 +0600

    Time: 104 ms (92.59%), Space: 17.1 MB (96.49%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,16 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-#         result = collections.defaultdict(list) # {} normal dict doesn't allow list value that's why                                                         using tuple with defaultdict
-#         for s in strs:
-#             count = [0] * 26 #count all the character in word
-
-#             for c in s:
-#                 count[ord(c) - ord("a")] += 1
-#             result[tuple(count)].append(s)
-
-#         return result.values() 
-
         h = {}
         for word in strs:
             sortedWord = ''.join(sorted(word))
