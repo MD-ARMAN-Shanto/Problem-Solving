67b53109ef66239e20d8094dffb57ec6d9face73
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-07-05 23:07:39 +0600

    Time: 3258 ms (17.31%), Space: 28.6 MB (42.76%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,40 +1,41 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
-        if not nums:
-            return 0
+#         if not nums:
+#             return 0
         
-        nums.sort()
+#         nums.sort()
         
-        longest_strike = 1
-        current_strike = 1 
+#         longest_strike = 1
+#         current_strike = 1 
         
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                if nums[i] == nums[i-1]+1:
-                    current_strike += 1
-                else:
-                    longest_strike = max(current_strike, longest_strike)
-                    current_strike = 1
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[i-1]:
+#                 if nums[i] == nums[i-1]+1:
+#                     current_strike += 1
+#                 else:
+#                     longest_strike = max(current_strike, longest_strike)
+#                     current_strike = 1
                     
-        return max(longest_strike, current_strike)
+#         return max(longest_strike, current_strike)
     
         # 2nd approch creating subsequece
         
-#         nSet = set(nums)
-#         longest = 0
+        nSet = set(nums)
+        longest = 0
         
-#         for num in nums:
-#             # check num-1 value not in set
-#             if (num-1) not in nSet:
-#                 length += 1
-#                 # check num+length = next_value in set
-#                 while (num+length) in nSet:
-#                     length += 1
-#                 # assign the longest subsequence value
-#                 longest = max(longest, length)
+        for num in nums:
+            length = 0
+            # check num-1 value not in set
+            if (num-1) not in nSet:
+                length += 1
+                # check num+length = next_value in set
+                while (num+length) in nSet:
+                    length += 1
+                # assign the longest subsequence value
+                longest = max(longest, length)
                 
-#         return longest
+        return longest
                 
                 
                 
