ca23e4c0816c0c9a2f249a719eedbc61c5c45d1b
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-07-12 14:13:27 +0600

    Time: 48 ms (44.18%), Space: 13.9 MB (22.51%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wordPattern(self, p: str, s: str) -> bool:
+        
+        l = s.split(' ')
+        
+        if len(p) != len(l):
+            return False
+        
+        d = {}
+        se = set()
+        
+        for i in range(len(l)):
+            x = p[i]
+            y = l[i]
+            
+            if x not in d:
+                if y in se:
+                    return False
+                d[x] = y
+                se.add(y)
+                
+            else:
+                if d[x] != y:
+                    return False
+                
+        return True
