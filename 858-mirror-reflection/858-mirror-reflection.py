abc9f3f4f9c2a045f915edc4dc4061dbc01cd19f
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-08-04 15:38:21 +0600

    Time: 37 ms (83.17%), Space: 13.9 MB (16.83%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.py
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -1,18 +1,12 @@
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
         
-        l = math.lcm(p, q)
+        L = math.lcm(p, q)
         
-        x = (l // q) % 2 # x axis
-        y = (l // p) % 2  # y axis
-        
-        
-        if x == 1 and y == 0:
-            return 0
-        elif x == 1 and y == 1:
-            return 1
-        else:
+        if (L//q)%2 == 0:
             return 2
+
+        return (L//p)%2
         
         
         
