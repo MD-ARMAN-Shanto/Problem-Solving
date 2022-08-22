fcc4d8d5c61c1ee5ecea25793b2c555249033807
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-08-22 11:23:52 +0600

    Time: 164 ms (8.71%), Space: 14.3 MB (96.07%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -4,24 +4,22 @@ class Solution:
         stack = []
         while len(tokens) > 0:
             toke = tokens.pop(0)
-            if toke == "+":
-                first = stack.pop()
-                second = stack.pop()
-                stack.append(second+first)
-            elif toke == "-":
-                first = stack.pop()
-                second = stack.pop()
-                stack.append(second-first) 
-            elif toke == "*":
-                first = stack.pop()
-                second = stack.pop()
-                stack.append(second*first)
-            elif toke == "/":
-                first = stack.pop()
-                second = stack.pop()
-                stack.append(int(float(second)/first))
-            else:
+            
+            if toke not in ['+', '-', '*', '/']:
                 stack.append(int(toke))
+            else:
+                first = stack.pop()
+                second = stack.pop()
+                if toke == "+":
+                    stack.append(second+first)
+                elif toke == "-":
+                    stack.append(second-first) 
+                elif toke == "*":
+                    stack.append(second*first)
+                elif toke == "/":
+                    stack.append(int(float(second)/first))
+            # else:
+            #     stack.append(int(toke))
         return stack.pop()
                     
         
