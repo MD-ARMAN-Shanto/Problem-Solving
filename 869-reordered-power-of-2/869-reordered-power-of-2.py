b9384ceeef88d5848dbb3f33519f35c073927025
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-08-28 16:15:33 +0600

    Time: 42 ms (83.89%), Space: 13.8 MB (58.84%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,24 +1,37 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         
-        l = list(str(n))
-        if l[0] == 0:
-            return False
+#         l = list(str(n))
+#         if l[0] == 0:
+#             return False
         
-        d = {}
+#         d = {}
         
-        for ch in l:
-            d[ch] = d.get(ch, 0) + 1
+#         for ch in l:
+#             d[ch] = d.get(ch, 0) + 1
         
-        rep = {}
-        for i in range(30):
-            p = 2 ** i
-            for ch in list(str(p)):
-                rep[ch] = rep.get(ch, 0) + 1
-            if rep == d:
-                return True
-            rep.clear()
+#         rep = {}
+#         for i in range(30):
+#             p = 2 ** i
+#             for ch in list(str(p)):
+#                 rep[ch] = rep.get(ch, 0) + 1
+#             if rep == d:
+#                 return True
+#             rep.clear()
             
+#         return False
+
+        counter_number = Counter(str(n))
+            
+        i = 1
+        iteration = 0
+        while iteration <= 30:
+            iteration += 1
+            tmp_i = str(i)
+            counter_tmp = Counter(tmp_i)
+            if counter_tmp == counter_number:
+                return True
+            i *= 2
         return False
                 
         
