fa59e74d50536febd367d269dda9064a509ab554
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-08-29 15:31:25 +0600

    Time: 1102 ms (28.11%), Space: 23.7 MB (98.31%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,32 +7,34 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-          # inorder solution
-#         def inorder(o, c):
-#             if o:
-#                 inorder(o.left, c.left)
-#                 if o is target:
-#                     self.ans = c
-#                 inorder(o.right, c.right)
+         
+        # inorder solution
+        def inorder(o, c):
+            if o:
+                inorder(o.left, c.left)
+                if o is target:
+                    self.ans = c
+                inorder(o.right, c.right)
         
-#         inorder(original, cloned)
-#         return self.ans
+        inorder(original, cloned)
+        return self.ans
+    
         # preorder solution
     
-        def preorder(node):
-            if node.val == target.val:
-                self.result = node
+#         def preorder(node):
+#             if node.val == target.val:
+#                 self.result = node
                 
-            if node.left != None:
-                preorder(node.left)
+#             if node.left != None:
+#                 preorder(node.left)
                 
-            if node.right != None:
-                preorder(node.right)
+#             if node.right != None:
+#                 preorder(node.right)
                 
-        if cloned:
-            preorder(cloned)
+#         if cloned:
+#             preorder(cloned)
             
-        return self.result
+#         return self.result
                 
                 
                 
