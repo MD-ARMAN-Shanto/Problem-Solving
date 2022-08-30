b69107c7dd90aec05582d03ff688d51d5b922856
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-08-30 12:29:28 +0600

    Time: 66 ms (24.76%), Space: 13.9 MB (74.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -8,7 +8,7 @@ class Solution:
     
         for i in range(n//2 + n%2):
             for j in range(n//2):
-                tmp = mat[n-1-j][i] #7
+                tmp = mat[n-1-j][i] 
                 mat[n-1-j][i] = mat[n-1-i][n-j-1]
                 mat[n-1-i][n-j-1] = mat[j][n-1-i]
                 mat[j][n-1-i] = mat[i][j]
