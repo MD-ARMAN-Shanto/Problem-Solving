e05ed93e680972920abd161fbea28634a5703827
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-08-30 11:45:17 +0600

    Time: 37 ms (92.73%), Space: 13.8 MB (74.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -12,7 +12,7 @@ class Solution:
             for j in range(i, len(mat)):
                 mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
                 
-        
+        # reverse the list with half of the length
         for r in range(len(mat)):
             for c in range(0, l//2):
                 mat[r][c], mat[r][l-c-1] = mat[r][l-c-1], mat[r][c]
