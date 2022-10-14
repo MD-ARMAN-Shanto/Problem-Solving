8344512dd1c6c24881453a85754f86dc2d04cd0b
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-10-15 02:51:30 +0600

    Time: 4095 ms (30.68%), Space: 60.6 MB (79.61%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -9,18 +9,29 @@ class Solution:
         if head.next == None:
             return None
         
-        count = 0
-        p1 = p2 = head
+#         count = 0
+#         p1 = p2 = head
         
-        while p1:
-            count += 1
-            p1 = p1.next
+#         while p1:
+#             count += 1
+#             p1 = p1.next
             
-        middle_index = count // 2
+#         middle_index = count // 2
         
-        for _ in range(middle_index - 1):
-            p2 = p2.next
+#         for _ in range(middle_index - 1):
+#             p2 = p2.next
             
         
-        p2.next = p2.next.next
+#         p2.next = p2.next.next
+#         return head
+
+#         slow and fast pointer
+        slow, fast = head, head.next.next
+    
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        slow.next = slow.next.next
+        
         return head
