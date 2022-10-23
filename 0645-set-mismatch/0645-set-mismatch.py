f35ecb3f1ebb0a8de0b3e535935518b88a737117
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2022-10-23 23:53:50 +0600

    Time: 210 ms (91.87%), Space: 16.4 MB (6.35%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        
+        nums.sort()
+        
+        seen = set()
+        res = []
+        
+        for num in nums:
+            if num in seen:
+                res.append(num)
+            seen.add(num)
+        
+        set_l = set(nums)
+        for i in range(1, len(nums)+1):
+            if i not in set_l:
+                res.append(i)
+                break
+                
+        return res
+                
+        
