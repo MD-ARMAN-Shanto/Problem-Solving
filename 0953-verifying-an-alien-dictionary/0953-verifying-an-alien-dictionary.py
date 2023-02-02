32122bbfe646374bdbe67523f28e9d2cd09d8f4f
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2023-02-02 13:18:50 +0600

    Time: 43 ms (50.46%), Space: 13.9 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -7,6 +7,7 @@ class Solution:
             word1 = words[i]
             word2 = words[i + 1]
 
+            
             for j in range(min(len(word1), len(word2))):
                 if word1[j] != word2[j]:
                     if order_index[word1[j]] > order_index[word2[j]]:
@@ -15,5 +16,6 @@ class Solution:
             else:
                 if len(word1) > len(word2):
                     return False
+
 
         return True
