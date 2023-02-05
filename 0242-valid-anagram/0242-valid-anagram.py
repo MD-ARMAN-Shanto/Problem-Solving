d3f7961a6c09cbf7a5e23c620e88da7b64d79aff
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2023-02-05 15:23:20 +0600

    Time: 53 ms (65.86%), Space: 14.4 MB (96.60%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        d = {}
+        
+        for ch in s:
+            d[ch] = d.get(ch, 0) + 1
+        
+        
+        for ch in t:
+            if ch in d and d[ch] > 0: 
+                d[ch] -= 1
+            else:
+                return False
+
+        return True
