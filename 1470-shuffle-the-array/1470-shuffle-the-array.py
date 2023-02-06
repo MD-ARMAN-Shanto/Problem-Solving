973f531e993f24f66d9732e256edf67148d86c2c
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2023-02-06 15:03:20 +0600

    Time: 68 ms (47.71%), Space: 14.1 MB (33.52%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,15 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         
-        x = nums[:n]
-        y = nums[n:2*n]
-        
-        result = []
+        res = []
         
         for i in range(n):
-            result.append(x[i])
-            result.append(y[i])
-            
-        return result
+            res += [nums[i], nums[i+n]]
+             
+        return res
             
         
