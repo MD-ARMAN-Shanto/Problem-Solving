b9712d1a5739af3c191deef569cda8001cc09845
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2023-02-15 12:23:40 +0600

    Time: 848 ms (19.45%), Space: 15.1 MB (65.15%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        
+        result = 0
+        
+        for digit in num:
+            result = 10 * result + digit
+            
+        
+        result += k
+        result_array = []
+        
+        while result:
+            d = result % 10
+            result_array.append(d)
+            result //= 10
+            
+        return result_array[::-1]
