5c2f878c5e863e3d0735306b0097661d06c75d1a
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2023-02-15 12:25:36 +0600

    Time: 428 ms (35.66%), Space: 15.2 MB (36.53%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -6,12 +6,14 @@ class Solution:
         for digit in num:
             result = 10 * result + digit
             
-        result += k
-        result_array = []
+#         result += k
+#         result_array = []
         
-        while result:
-            d = result % 10
-            result_array.append(d)
-            result //= 10
+#         while result:
+#             d = result % 10
+#             result_array.append(d)
+#             result //= 10
             
-        return result_array[::-1]
+#         return result_array[::-1]
+    
+        return [int(x) for x in str(result+k)]
