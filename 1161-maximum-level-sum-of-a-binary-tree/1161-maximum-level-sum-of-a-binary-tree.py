13c7e4d3a9e3c1802d3aeb7ab4638b49b0949cca
Arman_Shanto <42847894+MD-ARMAN-Shanto@users.noreply.github.com>
2023-06-15 16:43:03 +0600

    Time: 302 ms (70.67%), Space: 20.9 MB (85.96%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -7,21 +7,45 @@
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         
-        def dfs(node, level):
-            if not node:
-                return 0
+#         def dfs(node, level):
+#             if not node:
+#                 return 0
             
-            level_sum[level] += node.val
+#             level_sum[level] += node.val
             
-            dfs(node.left, level+1)
-            dfs(node.right, level+1)
+#             dfs(node.left, level+1)
+#             dfs(node.right, level+1)
         
-        level_sum = collections.defaultdict(int)
-        dfs(root, 1)
-        ans, maximum = 0, float("-inf")
+#         level_sum = collections.defaultdict(int)
+#         dfs(root, 1)
+#         ans, maximum = 0, float("-inf")
         
-        for level, total in level_sum.items():
-            if maximum < total:
-                ans , maximum = level, total
+#         for level, total in level_sum.items():
+#             if maximum < total:
+#                 ans , maximum = level, total
             
+#         return ans
+
+        max_sum = float("-inf")
+        q, level, ans = deque(), 0, 0
+        q.append(root)
+        
+        while q:
+            current_sum = 0
+            level += 1
+            
+            for _ in range(len(q)):
+                node = q.popleft()
+                current_sum += node.val
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                    
+            if current_sum > max_sum:
+                max_sum = current_sum
+                ans = level
+                
+                
         return ans
